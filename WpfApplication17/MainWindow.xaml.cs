2beab7d1a06dc46a8d66168347b198e2f87a5d2e
markxavier1 <markxavier1@outlook.com>
2017-02-27 11:54:14 +0000

    first commit

---
+++ WpfApplication17/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            Globel.mainwindow = this;
+            InitializeComponent();
+
+            PageRegister pagereg = new PageRegister();
+
+            FrameNavigate.Navigate(pagereg);
+
+        }
+
+
+
+
+        public void NavigatePages(string pagename)
+        {
+
+            PageRegister pagereg =null;
+            switch (pagename)
+            {
+
+           
+                case "PageRegister":
+                    if (pagereg == null)
+                    {
+                        pagereg = new PageRegister();
+                    }
+                    this.FrameNavigate.Navigate(pagereg);
+                    break;
+
+
+                
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+          
+
+        }
+    }
+}
---
+++ WpfApplication17/globel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication17
+{
+   static class Globel
+    {
+
+        public static MainWindow mainwindow = null;
+    }
+}
